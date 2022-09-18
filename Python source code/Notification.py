cbbe10f7da0b756b2e356f85bc9c3bf0623d4dca
Yash12007 <106073201+Yash12007@users.noreply.github.com>
2022-09-18 16:41:23 +0530

    Create Notification.py

---
+++ Python source code/Notification.py
@@ -0,0 +1,10 @@
+from plyer import notification
+
+
+if __name__=="__main__":
+ 
+        notification.notify(
+            title = "Example title",
+            message="Example massage" ,
+            timeout=2
+        )
